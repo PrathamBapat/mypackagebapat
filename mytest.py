92309eb26386c43cf178fb3f4e131e3d0ff56bec
PrathamBapat <201551075@iiitvadodara.ac.in>
2019-03-06 14:26:41 +0530

    basic files

--- mytest.py
+++
@@ -1,3 +0,0 @@
-import pysimplib.mymath as random
-print random.addition(1,2)
-
--- numpy_first.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-arr = np.array( [[1, 2, 3],
-                 [4, 5, 6]] )
-print "Array is of type: ", type(arr) 
-print "Array stores elements of type: ", arr.dtype
