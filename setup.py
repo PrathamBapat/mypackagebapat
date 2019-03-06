8162bbab90facba2d49697037af78414bd4ada39
Prathamesh Bapat <47621188+PrathamBapat@users.noreply.github.com>
2019-03-06 18:38:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'mypackagebapat',         # How you named your package folder (MyLib)
   packages = ['mypackagebapat'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A very basic addition and subtraction package',   # Give a short description about your library
   author = 'Prathamesh Bapat',                   # Type in your name
   author_email = 'prathameshbapat7757@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/PrathamBapat/mypackagebapat',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/PrathamBapat/mypackagebapat/archive/v_02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/PrathamBapat/mypackagebapat/archive/v_03.tar.gz',    # I explain this later on
   keywords = ['addition', 'subtraction'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
